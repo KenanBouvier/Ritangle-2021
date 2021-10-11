b10471a522da6019ff46ec1fc27fd20f0f167b9e
KenanBouvier <Kenanbouvier@gmail.com>
2021-10-12 00:05:16 +0100

    added files

--- Stage-1/question_1.cpp
+++ Stage-1/question_1.cpp
@@ -18,9 +18,7 @@ int main()
 			if (six < 2) {
 				six++;
 			}
-			else {
-				continue;
-			}
+			else continue;
 		}
 		else {
 			if (used.find(a) != used.end()) {
@@ -33,9 +31,7 @@ int main()
 				if (six < 2) {
 					six++;
 				}
-				else {
-					continue;
-				}
+				else continue;
 			}
 			else {
 				if (used.find(b) != used.end()) {
@@ -48,9 +44,7 @@ int main()
 					if (six < 2) {
 						six++;
 					}
-					else {
-						continue;
-					}
+					else continue;
 				}
 				else {
 					if (used.find(c) != used.end()) {
@@ -63,9 +57,7 @@ int main()
 						if (six < 2) {
 							six++;
 						}
-						else {
-							continue;
-						}
+						else continue;
 					}
 					else {
 						if (used.find(d) != used.end()) {
@@ -78,9 +70,7 @@ int main()
 							if (six < 2) {
 								six++;
 							}
-							else {
-								continue;
-							}
+							else continue;
 						}
 						else {
 							if (used.find(e) != used.end()) {
@@ -93,9 +83,7 @@ int main()
 								if (six < 2) {
 									six++;
 								}
-								else {
-									continue;
-								}
+								else continue;
 							}
 							else {
 								if (used.find(f) != used.end()) {
@@ -108,9 +96,7 @@ int main()
 									if (six < 2) {
 										six++;
 									}
-									else {
-										continue;
-									}
+									else continue;
 								}
 								else {
 									if (used.find(g) != used.end()) {
@@ -123,9 +109,7 @@ int main()
 										if (six < 2) {
 											six++;
 										}
-										else {
-											continue;
-										}
+										else continue;
 									}
 									else {
 										if (used.find(h) != used.end()) {
@@ -138,9 +122,7 @@ int main()
 											if (six < 2) {
 												six++;
 											}
-											else {
-												continue;
-											}
+											else continue;
 										}
 										else {
 											if (used.find(i) != used.end()) {
@@ -158,113 +140,47 @@ int main()
 										string fed = gc(f) + gc(e) + gc(d);
 										string ihg = gc(i) + gc(h) + gc(g);
 
-										// cout << abc << " " << def << " " << ghi << endl;
-
 										if (stoi(abc) + stoi(def) == stoi(ghi) && stoi(cba) + stoi(fed) == stoi(ihg)) {
 											// cout << abc << " " << def << " " << ghi << " is a solution!" << endl;
 											answers.push_back({abc, def, ghi});
 										}
 
-										if (i == 6) {
-											six--;
-										}
-										else {
-											used.erase(i);
-										}
+										if (i == 6) six--;
+										else used.erase(i);
 									}
-									if (h == 6) {
-										six--;
-									}
-									else {
-										used.erase(h);
-									}
+									if (h == 6) six--;
+									else used.erase(h);
 								}
-								if (g == 6) {
-									six--;
-								}
-								else {
-									used.erase(g);
-								}
+								if (g == 6) six--;
+								else used.erase(g);
 							}
-							if (f == 6) {
-								six--;
-							}
-							else {
-								used.erase(f);
-							}
+							if (f == 6) six--;
+							else used.erase(f);
 						}
-						if (e == 6) {
-							six--;
-						}
-						else {
-							used.erase(e);
-						}
+						if (e == 6) six--;
+						else used.erase(e);
 					}
-					if (d == 6) {
-						six--;
-					}
-					else {
-						used.erase(d);
-					}
+					if (d == 6) six--;
+					else used.erase(d);
 				}
-				if (c == 6) {
-					six--;
-				}
-				else {
-					used.erase(c);
-				}
+				if (c == 6) six--;
+				else used.erase(c);
 			}
-			if (b == 6) {
-				six--;
-			}
-			else {
-				used.erase(b);
-			}
+			if (b == 6) six--;
+			else used.erase(b);
 		}
-		if (a == 6) {
-			six--;
-		}
-		else {
-			used.erase(a);
-		}
+		if (a == 6) six--;
+		else used.erase(a);
 	}
 
-	//end of madness
-	cout << answers.size() << " number of answers" << endl;
+	// end of madness
+	cout << answers.size() << " number of solutions" << endl;
 	for (vector<string> v : answers) {
 		for (string s : v) {
 			cout << s << " ";
 		}
 		cout << endl;
 	}
-
-	/* Result:  format: ABC DEF GHI
-
-	16 number of answers
-	102 365 467
-	105 362 467
-	162 305 467
-	165 302 467
-	201 563 764
-	203 561 764
-	261 503 764
-	263 501 764
-	302 165 467
-	305 162 467
-	362 105 467
-	365 102 467
-	501 263 764
-	503 261 764
-	561 203 764
-	563 201 764
-
-	We can see that despite the multiple solutions, the values of G,H,I are 4,6,7 in some order.
-	Multiplying them out you get 168.
-	From there you can multiply by the given value, 447.2, where you get 75129.6
-	Taking int of that value you get 75129
-
-	*/
-
 
 	return 0;
 }
