68d7884b6b20b00db4a1adf4092d1eddf0c66168
KenanBouvier <Kenanbouvier@gmail.com>
2021-11-04 00:23:16 +0000

    question_2

--- Stage-1/question_2.cpp
+++ Stage-1/question_2.cpp
@@ -12,12 +12,10 @@ bool isTrue(long int triangle, long int square, long int fib) {
 		return false;
 	}
 	// triangle number good
-
 	if (!checkPS(square)) {
 		return false;
 	}
 	//square number good
-
 	if (checkPS(5 * fib * fib + 4) || checkPS(5 * fib * fib - 4)) {
 		return true;
 	}
@@ -29,7 +27,6 @@ int main() {
 	for (int a = 1; a < 100; ++a) {
 		for (int b = 1; b < 100; ++b) {
 			for (int c = 1; c < 100; ++c) {
-
 				vector<vector<int>> perms = {
 					{a, b, c},
 					{a, c, b},
@@ -38,11 +35,12 @@ int main() {
 					{c, a, b},
 					{c, b, a}
 				};
-
 				if (isTrue(a, b, c)) {
 					for (vector<int> p : perms) {
-						if (isTrue(p[0] * 9, p[1] * 9, p[2] * 9)) {
-							cout << a << " " << b << " " << c << " are the original ages\n";
+						if (isTrue(p[0] + 9, p[1] + 9, p[2] + 9)) {
+							if (a < b && b < c && b - a >= 16 & c - b >= 16) {
+								cout << a << " " << b << " " << c << " are the original ages\n";
+							}
 						}
 					}
 				}
